de8b261b6ffb6cfaf32f2ec62113a33210316a5a
Achmad Fauzan <91662639+achmdfzan@users.noreply.github.com>
2022-05-19 10:12:42 +0700

    db awal udah di insert dikit

--- db_mbkm.sql
+++ db_mbkm.sql
@@ -1,142 +1,214 @@
-/*==============================================================*/
-/* DBMS name:      MySQL 5.0                                    */
-/* Created on:     19/05/2022 09:14:23                          */
-/*==============================================================*/
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 19, 2022 at 05:11 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- alter table if exists KONTRAKMATKUL 
---    drop foreign key FK_KONTRAKM_KONTRAKMA_TMATKUL;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- alter table if exists KONTRAKMATKUL 
---    drop foreign key FK_KONTRAKM_KONTRAKMA_TMAHASIS;
+--
+-- Database: `db_mbkm`
+--
 
--- alter table if exists KONTRAKMBKM 
---    drop foreign key FK_KONTRAKM_KONTRAKMB_TMAHASIS;
+-- --------------------------------------------------------
 
--- alter table if exists KONTRAKMBKM 
---    drop foreign key FK_KONTRAKM_KONTRAKMB_TPROGRAM;
+--
+-- Table structure for table `kontrakmatkul`
+--
 
--- alter table if exists TMAHASISWA 
---    drop foreign key FK_TMAHASIS_MEMBIMBIN_TPEMBIMB;
+CREATE TABLE `kontrakmatkul` (
+  `KODE_MK` varchar(10) NOT NULL,
+  `NIM` varchar(10) NOT NULL,
+  `NILAI` float DEFAULT NULL,
+  `ID_KONTRAKMATKUL` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
 
--- alter table if exists KONTRAKMATKUL 
---    drop foreign key FK_KONTRAKM_KONTRAKMA_TMATKUL;
+--
+-- Table structure for table `kontrakmbkm`
+--
 
--- alter table if exists KONTRAKMATKUL 
---    drop foreign key FK_KONTRAKM_KONTRAKMA_TMAHASIS;
+CREATE TABLE `kontrakmbkm` (
+  `NIM` varchar(10) NOT NULL,
+  `ID_PROGRAM` varchar(10) NOT NULL,
+  `STATUS` varchar(20) NOT NULL,
+  `ID_KONTRAKMBKM` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- drop table if exists KONTRAKMATKUL;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `tmahasiswa`
+--
 
--- alter table if exists KONTRAKMBKM 
---    drop foreign key FK_KONTRAKM_KONTRAKMB_TMAHASIS;
+CREATE TABLE `tmahasiswa` (
+  `NIP` varchar(20) NOT NULL,
+  `NIM` varchar(10) NOT NULL,
+  `NAMA_MAHASISWA` varchar(50) NOT NULL,
+  `PRODI` varchar(50) NOT NULL,
+  `EMAIL_MAHASISWA` varchar(50) NOT NULL,
+  `SEMESTER_MAHASISWA` int(11) NOT NULL,
+  `SKS_AKUMULATIF` int(11) DEFAULT 0,
+  `IPK_MAHASISWA` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- alter table if exists KONTRAKMBKM 
---    drop foreign key FK_KONTRAKM_KONTRAKMB_TPROGRAM;
+--
+-- Dumping data for table `tmahasiswa`
+--
 
--- drop table if exists KONTRAKMBKM;
+INSERT INTO `tmahasiswa` (`NIP`, `NIM`, `NAMA_MAHASISWA`, `PRODI`, `EMAIL_MAHASISWA`, `SEMESTER_MAHASISWA`, `SKS_AKUMULATIF`, `IPK_MAHASISWA`) VALUES
+('22222222', '2102585', 'Apri Anggara Yudha', 'Ilmu Komputer', 'aprianggarayudha585@upi.edu', 5, 0, 4),
+('33333333', '2102843', 'Najma Qalbi Dwiharani', 'Ilmu Komputer', 'najmadwiharani@upi.edu', 6, 0, 4),
+('11111111', '2108061', 'Achmad Fauzan', 'Ilmu Komputer', 'achmdfzan@upi.edu', 5, 0, 3.96);
 
+-- --------------------------------------------------------
 
--- alter table if exists TMAHASISWA 
---    drop foreign key FK_TMAHASIS_MEMBIMBIN_TPEMBIMB;
+--
+-- Table structure for table `tmatkul`
+--
 
--- drop table if exists TMAHASISWA;
+CREATE TABLE `tmatkul` (
+  `KODE_MK` varchar(10) NOT NULL,
+  `NAMA_MK` varchar(50) NOT NULL,
+  `SKS_MK` int(11) NOT NULL,
+  `SEMESTER_MK` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- drop table if exists TMATKUL;
+--
+-- Dumping data for table `tmatkul`
+--
 
--- drop table if exists TPEMBIMBING;
+INSERT INTO `tmatkul` (`KODE_MK`, `NAMA_MK`, `SKS_MK`, `SEMESTER_MK`) VALUES
+('IK310', 'Kriptografi', 3, 5),
+('IK320', 'Kecerdasan Buatan', 5, 3),
+('IK330', 'Algoritma dan Pemrograman 5', 3, 3);
 
--- drop table if exists TPROGRAMMBKM;
+-- --------------------------------------------------------
 
-/*==============================================================*/
-/* Table: KONTRAKMATKUL                                         */
-/*==============================================================*/
-create table KONTRAKMATKUL
-(
-   KODE_MK              varchar(10) not null  comment '',
-   NIM                  varchar(10) not null  comment '',
-   NILAI                float  comment '',
-   ID_KONTRAKMATKUL     int not null  comment '',
-   primary key (ID_KONTRAKMATKUL)
-);
+--
+-- Table structure for table `tpembimbing`
+--
 
-/*==============================================================*/
-/* Table: KONTRAKMBKM                                           */
-/*==============================================================*/
-create table KONTRAKMBKM
-(
-   NIM                  varchar(10) not null  comment '',
-   ID_PROGRAM           varchar(10) not null  comment '',
-   STATUS               varchar(20) not null  comment '',
-   ID_KONTRAKMBKM       int not null  comment '',
-   primary key (ID_KONTRAKMBKM)
-);
+CREATE TABLE `tpembimbing` (
+  `NIP` varchar(20) NOT NULL,
+  `NAMA_PEMBIMBING` varchar(50) NOT NULL,
+  `EMAIL_PEMBIMBING` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*==============================================================*/
-/* Table: TMAHASISWA                                            */
-/*==============================================================*/
-create table TMAHASISWA
-(
-   NIP                  varchar(20) not null  comment '',
-   NIM                  varchar(10) not null  comment '',
-   NAMA_MAHASISWA       varchar(50) not null  comment '',
-   PRODI                varchar(50) not null  comment '',
-   EMAIL_MAHASISWA      varchar(50) not null  comment '',
-   SEMESTER_MAHASISWA   int not null  comment '',
-   SKS_AKUMULATIF       int not null  comment '',
-   IPK_MAHASISWA        float not null  comment '',
-   primary key (NIM)
-);
+--
+-- Dumping data for table `tpembimbing`
+--
 
-/*==============================================================*/
-/* Table: TMATKUL                                               */
-/*==============================================================*/
-create table TMATKUL
-(
-   KODE_MK              varchar(10) not null  comment '',
-   NAMA_MK              varchar(50) not null  comment '',
-   SKS_MK               int not null  comment '',
-   SEMESTER_MK          int not null  comment '',
-   primary key (KODE_MK)
-);
+INSERT INTO `tpembimbing` (`NIP`, `NAMA_PEMBIMBING`, `EMAIL_PEMBIMBING`) VALUES
+('11111111', 'Dr. Rani Megasari, M.T.', 'megasari.upi.edu'),
+('22222222', 'Rosa Ariani Sukamto,  M.T.', 'rosa.ariani@upi.edu'),
+('33333333', 'Dr. Asep Wahyudin, M.T.', 'away@upi.edu');
 
-/*==============================================================*/
-/* Table: TPEMBIMBING                                           */
-/*==============================================================*/
-create table TPEMBIMBING
-(
-   NIP                  varchar(20) not null  comment '',
-   NAMA_PEMBIMBING      varchar(50) not null  comment '',
-   EMAIL_PEMBIMBING     varchar(50) not null  comment '',
-   primary key (NIP)
-);
+-- --------------------------------------------------------
 
-/*==============================================================*/
-/* Table: TPROGRAMMBKM                                          */
-/*==============================================================*/
-create table TPROGRAMMBKM
-(
-   ID_PROGRAM           varchar(10) not null  comment '',
-   NAMA_PROGRAM         varchar(10) not null  comment '',
-   JENIS_PROGRAM        varchar(10) not null  comment '',
-   DURASI               int not null  comment '',
-   SKS_PROGRAM          int not null  comment '',
-   primary key (ID_PROGRAM)
-);
+--
+-- Table structure for table `tprogrammbkm`
+--
 
-alter table KONTRAKMATKUL add constraint FK_KONTRAKM_KONTRAKMA_TMATKUL foreign key (KODE_MK)
-      references TMATKUL (KODE_MK) on delete restrict on update restrict;
+CREATE TABLE `tprogrammbkm` (
+  `ID_PROGRAM` varchar(10) NOT NULL,
+  `NAMA_PROGRAM` varchar(50) NOT NULL,
+  `JENIS_PROGRAM` varchar(50) NOT NULL,
+  `DURASI` int(11) NOT NULL,
+  `SKS_PROGRAM` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-alter table KONTRAKMATKUL add constraint FK_KONTRAKM_KONTRAKMA_TMAHASIS foreign key (NIM)
-      references TMAHASISWA (NIM) on delete restrict on update restrict;
+--
+-- Dumping data for table `tprogrammbkm`
+--
 
-alter table KONTRAKMBKM add constraint FK_KONTRAKM_KONTRAKMB_TMAHASIS foreign key (NIM)
-      references TMAHASISWA (NIM) on delete restrict on update restrict;
+INSERT INTO `tprogrammbkm` (`ID_PROGRAM`, `NAMA_PROGRAM`, `JENIS_PROGRAM`, `DURASI`, `SKS_PROGRAM`) VALUES
+('11111', 'Magang di Google', 'Magang', 6, 20),
+('22222', 'KKN di desa Konoha', 'KKN', 6, 20),
+('33333', 'Pertukaran Mahasiswa ke Harvard', 'Pertukaran Mahasiswa', 6, 20);
 
-alter table KONTRAKMBKM add constraint FK_KONTRAKM_KONTRAKMB_TPROGRAM foreign key (ID_PROGRAM)
-      references TPROGRAMMBKM (ID_PROGRAM) on delete restrict on update restrict;
+--
+-- Indexes for dumped tables
+--
 
-alter table TMAHASISWA add constraint FK_TMAHASIS_MEMBIMBIN_TPEMBIMB foreign key (NIP)
-      references TPEMBIMBING (NIP) on delete restrict on update restrict;
+--
+-- Indexes for table `kontrakmatkul`
+--
+ALTER TABLE `kontrakmatkul`
+  ADD PRIMARY KEY (`ID_KONTRAKMATKUL`),
+  ADD KEY `FK_KONTRAKM_KONTRAKMA_TMATKUL` (`KODE_MK`),
+  ADD KEY `FK_KONTRAKM_KONTRAKMA_TMAHASIS` (`NIM`);
 
+--
+-- Indexes for table `kontrakmbkm`
+--
+ALTER TABLE `kontrakmbkm`
+  ADD PRIMARY KEY (`ID_KONTRAKMBKM`),
+  ADD KEY `FK_KONTRAKM_KONTRAKMB_TMAHASIS` (`NIM`),
+  ADD KEY `FK_KONTRAKM_KONTRAKMB_TPROGRAM` (`ID_PROGRAM`);
+
+--
+-- Indexes for table `tmahasiswa`
+--
+ALTER TABLE `tmahasiswa`
+  ADD PRIMARY KEY (`NIM`),
+  ADD KEY `FK_TMAHASIS_MEMBIMBIN_TPEMBIMB` (`NIP`);
+
+--
+-- Indexes for table `tmatkul`
+--
+ALTER TABLE `tmatkul`
+  ADD PRIMARY KEY (`KODE_MK`);
+
+--
+-- Indexes for table `tpembimbing`
+--
+ALTER TABLE `tpembimbing`
+  ADD PRIMARY KEY (`NIP`);
+
+--
+-- Indexes for table `tprogrammbkm`
+--
+ALTER TABLE `tprogrammbkm`
+  ADD PRIMARY KEY (`ID_PROGRAM`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `kontrakmatkul`
+--
+ALTER TABLE `kontrakmatkul`
+  ADD CONSTRAINT `FK_KONTRAKM_KONTRAKMA_TMAHASIS` FOREIGN KEY (`NIM`) REFERENCES `tmahasiswa` (`NIM`),
+  ADD CONSTRAINT `FK_KONTRAKM_KONTRAKMA_TMATKUL` FOREIGN KEY (`KODE_MK`) REFERENCES `tmatkul` (`KODE_MK`);
+
+--
+-- Constraints for table `kontrakmbkm`
+--
+ALTER TABLE `kontrakmbkm`
+  ADD CONSTRAINT `FK_KONTRAKM_KONTRAKMB_TMAHASIS` FOREIGN KEY (`NIM`) REFERENCES `tmahasiswa` (`NIM`),
+  ADD CONSTRAINT `FK_KONTRAKM_KONTRAKMB_TPROGRAM` FOREIGN KEY (`ID_PROGRAM`) REFERENCES `tprogrammbkm` (`ID_PROGRAM`);
+
+--
+-- Constraints for table `tmahasiswa`
+--
+ALTER TABLE `tmahasiswa`
+  ADD CONSTRAINT `FK_TMAHASIS_MEMBIMBIN_TPEMBIMB` FOREIGN KEY (`NIP`) REFERENCES `tpembimbing` (`NIP`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
